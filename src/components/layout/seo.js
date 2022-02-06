783f24d4b9c98757e7cbe16a2781d7f37e02c26e
Jack Sari <janasarii@gmail.com>
2022-02-06 09:22:22 -0500

    add seo v3

--- src/components/layout/seo.js
+++ src/components/layout/seo.js
@@ -24,18 +24,18 @@ const Seo = ({page, desc}) => {
     <meta name="description" content={desc || description}/>
 
     <meta name="image" content={image}/>
-    <meta name="keywords" content="Portafolio, Jack Sari, Jack Anthony Sánchez Rivera, React, Frontend"/>
+    <meta name="keywords" content="Jack Sari, Jack Anthony Sánchez Rivera, React, Frontend Developer, Portafolio, ReactJs, NextJs"/>
     <meta name="author" content={author} />
     <meta name="copyright" content="Propietario del copyright" />
     {/* twitter card*/}
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:creator" content={twitterUsername}/>
-    <meta name="twitter:title" content={title}/>
+    <meta name="twitter:title" content={`${page} | ${title}`}/>
     <meta name="twitter:description" content={desc || description}/>
     <meta name="twitter:image" content={`${image}`}/>
     {/* OpenGraph tags */}
     <meta property="og:url" content={siteUrl} />
-    <meta property="og:title" content={page} />
+    <meta property="og:title" content={`${page} | ${title}`} />
     <meta property="og:description" content={desc || description} />
     <meta property="og:image" content={image} />
     <meta property="fb:app_id" content='' />
